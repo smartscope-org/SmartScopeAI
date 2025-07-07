dfeba09203b4d96c8ab2e05ae0922de63a9652bb
Jonathan Bouvette <jonathan.bouvette@gmail.com>
2025-07-07 15:02:49 -0400

    added REDIS_PASSWORD env variable

--- SmartscopeAI/interfaces/celery/settings.py
+++ SmartscopeAI/interfaces/celery/settings.py
@@ -2,12 +2,18 @@ import os
 
 REDIS_HOST = os.getenv('REDIS_HOST', "cache")
 REDIS_PORT = os.getenv('REDIS_PORT', "6379")
+REDIS_PASSWORD = os.getenv('REDIS_PASSWORD', None)
 CALLBACK_SITE = os.getenv('CALLBACK_SITE', "http://localhost:8000") 
+
+if REDIS_PASSWORD:
+    REDIS_URL = f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}"
+else:
+    REDIS_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}"
 
 accept_content = ['json']
 result_accept_content = ['json']
-result_backend = f"redis://{REDIS_HOST}:{REDIS_PORT}/1"
-broker_url = f"redis://{REDIS_HOST}:{REDIS_PORT}/0"
+result_backend = f"{REDIS_URL}/1"
+broker_url = f"{REDIS_URL}/0"
 
 task_track_started = True
 
